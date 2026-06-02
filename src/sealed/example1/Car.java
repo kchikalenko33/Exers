160c304842348dbf051a07f6b4e2d2b15b37e680
kirill <kchikalenko33@gmail.com>
2026-06-02 23:31:15 +0300

    commit

--- src/sealed/example1/Car.java
+++ src/sealed/example1/Car.java
@@ -1,6 +1,12 @@
 package sealed.example1;
 
-final class Car extends Vehicle {
+/**
+ * Класс описывающий машину
+ */
+public final class Car extends Vehicle {
+    /**
+     * кол-во дверей
+     */
     private int doors;
 
     public Car(String brand, int year, int doors) {
@@ -8,12 +14,25 @@ final class Car extends Vehicle {
         this.doors = doors;
     }
 
+    /**
+     * Запускает двигатель
+     * @param horsePower - л.с
+     * @throws IllegalArgumentException - если л.с < 50 - error
+     */
+    public void startEngine(int horsePower) {
+        System.out.println("Автомобиль " + brand + " завёлся плавно.");
+        if (horsePower < 50) {
+            throw new IllegalArgumentException("error");
+        }
+    }
+
     @Override
     public void startEngine() {
-        System.out.println("Автомобиль " + brand + " завёлся плавно.");
+        super.startEngine();
     }
 
     public int getDoors() {
+        startEngine(33);
         return doors;
     }
 }
